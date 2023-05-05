ef7f0ca8b7f7e8a3357bbe295890f0283c42f6e4
Mohamed Derri <simoderri01@gmail.com>
2023-05-04 23:38:04 -0500

    fixing slow import issue

--- Labs/lab2/pcn_sequential.py
+++ Labs/lab2/pcn_sequential.py
@@ -1,12 +1,3 @@
-# Code from Chapter 2 of Machine Learning: An Algorithmic Perspective
-# by Stephen Marsland (http://seat.massey.ac.nz/personal/s.r.marsland/MLBook.html)
-
-# You are free to use, change, or redistribute the code in any way you wish for
-# non-commercial purposes, but please maintain the name of the original author.
-# This code comes with no warranty of any kind.
-
-# Stephen Marsland, 2008
-
 from numpy import *
 
 class pcn:
